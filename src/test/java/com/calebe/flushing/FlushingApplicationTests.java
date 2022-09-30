2f1c739ee3aa90749639a899a965f5b396da628c
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-09-30 10:14:22 -0300

    add JPA flush mode examples

--- src/test/java/com/calebe/flushing/FlushingApplicationTests.java
+++ src/test/java/com/calebe/flushing/FlushingApplicationTests.java
@@ -1,11 +1,13 @@
 package com.calebe.flushing;
 
+import com.calebe.flushing.domain.DifferentProduct;
 import com.calebe.flushing.domain.Product;
 import com.calebe.flushing.support.TransactionRunner;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.persistence.FlushModeType;
 import java.util.logging.Logger;
 
 @SpringBootTest
@@ -24,6 +26,63 @@ class FlushingApplicationTests {
 			LOGGER.info("Before flush");
 			entityManager.flush();
 			LOGGER.info("After flush");
+			entityManager.persist(p2);
+			LOGGER.info("Before Tx commit");
+		});
+	}
+
+	@Test
+	void testAutoFlushingWithJPQLQuerySameEntity() {
+		txRunner.executeInTransaction( entityManager -> {
+			Product p1 = new Product(1, "p1");
+			Product p2 = new Product(2, "p2");
+			entityManager.persist(p1);
+			LOGGER.info("Before JPQL query");
+			entityManager.createQuery("FROM Product", Product.class).getResultList();
+			LOGGER.info("After JPQL query");
+			entityManager.persist(p2);
+			LOGGER.info("Before Tx commit");
+		});
+	}
+
+	@Test
+	void testAutoFlushingWithJPQLQueryDifferentEntity() {
+		txRunner.executeInTransaction( entityManager -> {
+			Product p1 = new Product(1, "p1");
+			Product p2 = new Product(2, "p2");
+			entityManager.persist(p1);
+			LOGGER.info("Before JPQL query");
+			entityManager.createQuery("FROM DifferentProduct", DifferentProduct.class).getResultList();
+			LOGGER.info("After JPQL query");
+			entityManager.persist(p2);
+			LOGGER.info("Before Tx commit");
+		});
+	}
+
+	@Test
+	void testAutoFlushingWithNativeQuery() {
+		txRunner.executeInTransaction( entityManager -> {
+			Product p1 = new Product(1, "p1");
+			Product p2 = new Product(2, "p2");
+			entityManager.persist(p1);
+			LOGGER.info("Before native query");
+			entityManager.createNativeQuery("SELECT * FROM different_products").getResultList();
+			LOGGER.info("After native query");
+			entityManager.persist(p2);
+			LOGGER.info("Before Tx commit");
+		});
+	}
+
+	@Test
+	void testCommitFlushing() {
+		txRunner.executeInTransaction( entityManager -> {
+			entityManager.setFlushMode(FlushModeType.COMMIT);
+			Product p1 = new Product(1, "p1");
+			Product p2 = new Product(2, "p2");
+			entityManager.persist(p1);
+			LOGGER.info("Before native query");
+			entityManager.createNativeQuery("SELECT * FROM different_products").getResultList();
+			LOGGER.info("After native query");
 			entityManager.persist(p2);
 			LOGGER.info("Before Tx commit");
 		});
