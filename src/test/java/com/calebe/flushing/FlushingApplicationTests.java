ab5efbef06d212e64c0ed922e525f56166c9d67a
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-09-30 10:31:34 -0300

    add hibernate flush mode examples

--- src/test/java/com/calebe/flushing/FlushingApplicationTests.java
+++ src/test/java/com/calebe/flushing/FlushingApplicationTests.java
@@ -3,6 +3,8 @@ package com.calebe.flushing;
 import com.calebe.flushing.domain.DifferentProduct;
 import com.calebe.flushing.domain.Product;
 import com.calebe.flushing.support.TransactionRunner;
+import org.hibernate.FlushMode;
+import org.hibernate.Session;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -85,6 +87,37 @@ class FlushingApplicationTests {
 			LOGGER.info("After native query");
 			entityManager.persist(p2);
 			LOGGER.info("Before Tx commit");
+		});
+	}
+
+	@Test
+	void testAlwaysFlushingJPQLQueryDifferentEntity() {
+		txRunner.executeInTransaction( entityManager -> {
+			entityManager.unwrap(Session.class).setFlushMode(FlushMode.ALWAYS);
+			Product p1 = new Product(1, "p1");
+			Product p2 = new Product(2, "p2");
+			entityManager.persist(p1);
+			LOGGER.info("Before JPQL query");
+			entityManager.createQuery("FROM DifferentProduct", DifferentProduct.class).getResultList();
+			LOGGER.info("After JPQL query");
+			entityManager.persist(p2);
+			LOGGER.info("Before Tx commit");
+		});
+	}
+
+	@Test
+	void testManualFlushing() {
+		txRunner.executeInTransaction( entityManager -> {
+			entityManager.unwrap(Session.class).setFlushMode(FlushMode.MANUAL);
+			Product p1 = new Product(1, "p1");
+			Product p2 = new Product(2, "p2");
+			entityManager.persist(p1);
+			LOGGER.info("Before JPQL query");
+			entityManager.createNativeQuery("SELECT * FROM products").getResultList();
+			LOGGER.info("After JPQL query");
+			entityManager.persist(p2);
+			LOGGER.info("Before Tx commit");
+			entityManager.flush();
 		});
 	}
 
